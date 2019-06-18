378ef3d75d2c39865ad82f6d76ba200bc6ff0d04
Wolf <Wolf@DESKTOP-21SVAEG>
2019-06-18 15:25:11 +0900

    Quiz1

---
+++ Quiz1/Quiz1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Quiz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0) a += i;
+            }
+            Console.WriteLine("{0}", a);
+            Console.ReadLine();
+        }
+    }
+}
